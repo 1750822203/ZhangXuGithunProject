9446d404952be7868f56515850a80feddc2b6c62
Administrator <1750822203@qq.com>
2020-03-12 09:17:01 +0800

    HelloServlet has been modified

--- src/servlets/HelloServlet.java
+++ src/servlets/HelloServlet.java
@@ -19,7 +19,7 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
            PrintWriter out=resp.getWriter();
-           out.println("Hello word!");
+           out.println("Hello word!！！！");
 
     }
 }
